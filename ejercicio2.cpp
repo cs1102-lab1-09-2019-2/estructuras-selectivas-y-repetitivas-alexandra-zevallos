8fbca5bfa096f46ce075a476d340f7209d78969c
alexandra-zevallos <54292772+alexandra-zevallos@users.noreply.github.com>
2019-09-03 22:47:13 -0500

    Ejercicio 2 modificado

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,6 +1,29 @@
+#include <iostream>
 
+using std::cout;
+using std::cin;
+
+void imprimir(int &n) {
+  int d = 1;
+  for(int f = 1; f <= n; f++) {
+    for(int c = 1; c <= n; c++) {
+      if(f == 1 || f == n)
+        cout<< "*";
+      else {
+        if(c == 1 || c == n || c == d)
+          cout<< "*";
+        else 
+          cout<< " ";
+      }
+    }
+    d++;
+    cout<< "\n";
+  }
+}
 
 int main() {
-
-    return 0;
+  int n;
+  cout<< "Ingrese un numero entero positivo: ";
+  cin>> n;
+  imprimir(n);
 }
