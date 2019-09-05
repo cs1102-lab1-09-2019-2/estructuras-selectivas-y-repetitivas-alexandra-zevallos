a9327a5ddcf392ca9872066a270a8b83e3a0cef9
alexandra-zevallos <54292772+alexandra-zevallos@users.noreply.github.com>
2019-09-05 09:36:02 -0500

    Ejercicio 3 modificado

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int main() {
-  int r, c, i = 0;
-  vector <int> v;
+  int r, c, mayor = 0, i = 0;
   cout<< "Ingrese un numero entero positivo que representara la cantidad de datos que se ingresaran\n"<< "n: ";
   cin>> r;
   cout<< "Ahora ingrese los 'n' numeros: \n";
   while(i < r) {
     cin>> c;
-    v.push_back(c);
+    if(c > mayor)
+      mayor = c;
+    else
+      mayor = mayor;
     i++;
   }
-  sort(v.begin(),v.end());
-  cout<< "El maximo valor es "<< v[r-1];
+  cout<< "El maximo valor es "<< mayor;
+}
